7f83c1398b9c66cd6a0e8dbec06d67f366984e39
Florecita Wenny <h1101241039@student.untan.ac.id>
2026-03-28 16:56:23 +0700

    menambahkan log

--- apps/backend/src/auth.ts
+++ apps/backend/src/auth.ts
@@ -1,6 +1,7 @@
 import { google } from "googleapis";
 
 export function createOAuthClient() {
+  console.log("REDIRECT:", process.env.GOOGLE_REDIRECT_URI);
   return new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
